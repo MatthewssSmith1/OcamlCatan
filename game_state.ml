9543becbdf084df4545be2aa28058cbd327d7e41
Jason <jy469@cornell.edu>
2021-03-28 15:32:57 -0400

    Work on creating new games

--- game_state.ml
+++ game_state.ml
@@ -62,7 +62,7 @@ type game_state = {
   hexes: (vec2 * tHex) list;
   ports: (int * tPort) list;
   edges: (edge_coord * tEdge) list;
-  vericies: (vertex_coord * tVertex) list;
+  vertices: (vertex_coord * tVertex) list;
   inventories: (tTeam * inventory) list;
   trades: trade_offer list;
 }
@@ -73,3 +73,20 @@ type tMove =
   | BuildRoad of (tTeam * edge_coord)
   | BuildSettlement of (tTeam * vertex_coord)
   | BuildCity of (tTeam * vertex_coord)
+
+let to_vec2_list (input : (int * int) list) =
+  List.map (fun (a, b) -> {x = a; y = b}) input
+
+let rec list_merger list_1 list_2 =
+  match list_1, list_2 with
+  | [], [] -> []
+  | h1 :: t1, h2 :: t2 -> (h1, h2) :: list_merger t1 t2
+  | _, _ -> failwith "Dimensions not equal"
+
+let make_new_game (hex_list : tHex list) (ports : (int * tPort) list) =
+  let vec_order = to_vec2_list [(0, -2); (1, -2); (2, -2); (-1, -1); (0, -1);
+  (1, -1); (2, -1); (-2, 0); (-1, 0); (0, 0); (1, 0); (2, 0);
+  (-2, 1); (-1,1); (0, 1); (1, 1); (-2, 2); (-1, 2); (0, 2)] in
+  let hexes = list_merger vec_order hex_list in
+  {hexes = hexes; ports = ports; edges = []; vertices = [];
+  inventories = []; trades = []}
