6dc4ca2c3b533404ef5dc42021662305e57216c7
Jason <jy469@cornell.edu>
2021-04-01 17:24:12 -0400

    updated main

--- main.ml
+++ main.ml
@@ -1,11 +1,6 @@
-(* let rec play_game =
-  let input = read_line () in *)
-
-let main () = failwith "TODO"
-  (* let game = Game_state.make_new_game in
-  print_string 
-    "\n\nWelcome to the 3110 Ocaml Catan Game.\n";
-  print_endline *)
+let main () = (*failwith "TODO"*)
+  let game = Game_state.make_new_game in
+  Display.print_board game
   
 
 let () = main ()
