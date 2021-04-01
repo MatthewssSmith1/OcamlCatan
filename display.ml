9e5af7f7d158c0748c1af34dd5bc6ba06d43637e
Matt Smith <mss433@cornell.edu>
2021-04-01 12:26:37 -0400

    added .merlin file to import ANSITerminal, finished the function to print a raster to the console

--- display.ml
+++ display.ml
@@ -1,10 +1,16 @@
-let color_arr = [| ANSITerminal.red; ANSITerminal.green |]
+let color_arr =
+  [ ANSITerminal.red; ANSITerminal.green; ANSITerminal.yellow ]
+
+let color_of_int index =
+  match List.nth_opt color_arr index with
+  | None -> ANSITerminal.white
+  | Some c -> c
 
 let rec print_row (row : int list) =
   match row with
   | [] -> ()
   | hd :: tl ->
-      ANSITerminal.print_string [ color_arr.[hd] ] "██";
+      ANSITerminal.print_string [ color_of_int hd ] "██";
       print_row tl
 
 (** print_raster [r] prints [r] (a raster, or rectangular set of
@@ -14,6 +20,7 @@ let rec print_raster (raster : int list list) =
   | [] -> ()
   | hd :: tl ->
       print_row hd;
+      "" |> print_endline |> ignore;
       print_raster tl
 
 let print_board = failwith "Unimplemented"
