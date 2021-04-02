1d0e645a2b069ccc34cdb02d3507bae1c37d3be6
Jason <jy469@cornell.edu>
2021-04-02 16:13:44 -0400

    Cleaned up test.ml

--- test.ml
+++ test.ml
@@ -7,43 +7,44 @@ open Player
     First, they must both be {i set-like}, meaning that they do not
     contain any duplicates. Second, they must contain the same elements,
     though not necessarily in the same order. *)
-    let cmp_set_like_lists lst1 lst2 =
-      let uniq1 = List.sort_uniq compare lst1 in
-      let uniq2 = List.sort_uniq compare lst2 in
-      List.length lst1 = List.length uniq1
-      && List.length lst2 = List.length uniq2
-      && uniq1 = uniq2
-    
-    (** [pp_string s] pretty-prints string [s]. *)
-    let pp_string s = "\"" ^ s ^ "\""
-    
-    (** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt] to
-        pretty-print each element of [lst]. *)
-    let pp_list pp_elt lst =
-      let pp_elts lst =
-        let rec loop n acc = function
-          | [] -> acc
-          | [ h ] -> acc ^ pp_elt h
-          | h1 :: (h2 :: t as t') ->
-              if n = 100 then acc ^ "..." (* stop printing long list *)
-              else loop (n + 1) (acc ^ pp_elt h1 ^ "; ") t'
-        in
-        loop 0 "" lst
-      in
-      "[" ^ pp_elts lst ^ "]"
+let cmp_set_like_lists lst1 lst2 =
+  let uniq1 = List.sort_uniq compare lst1 in
+  let uniq2 = List.sort_uniq compare lst2 in
+  List.length lst1 = List.length uniq1
+  && List.length lst2 = List.length uniq2
+  && uniq1 = uniq2
 
-let check_resource_test (name : string) (resource : Game_state.resource)
-(player : Player.t) (expected_output : int) : test =
+(** [pp_string s] pretty-prints string [s]. *)
+let pp_string s = "\"" ^ s ^ "\""
+
+(** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt] to
+    pretty-print each element of [lst]. *)
+let pp_list pp_elt lst =
+  let pp_elts lst =
+    let rec loop n acc = function
+      | [] -> acc
+      | [ h ] -> acc ^ pp_elt h
+      | h1 :: (h2 :: t as t') ->
+          if n = 100 then acc ^ "..." (* stop printing long list *)
+          else loop (n + 1) (acc ^ pp_elt h1 ^ "; ") t'
+    in
+    loop 0 "" lst
+  in
+  "[" ^ pp_elts lst ^ "]"
+
+let check_resource_test
+    (name : string)
+    (resource : Types.resource)
+    (player : Player.t)
+    (expected_output : int) : test =
   name >:: fun _ ->
-  assert_equal expected_output (Player.check_resource resource 1 player)
-  ~printer: string_of_int
+  assert_equal expected_output
+    (Player.check_resource resource player)
+    ~printer:string_of_int
 
-let player_tests = [
-  
-]
+let player_tests = []
 
 let suite =
-  "test suite for OcamlCatan"
-  >::: List.flatten [ player_tests ]
-      
+  "test suite for OcamlCatan" >::: List.flatten [ player_tests ]
+
 let _ = run_test_tt_main suite
