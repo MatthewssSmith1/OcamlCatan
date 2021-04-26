121c3d1b16a1444e5962705e255228e38f395567
Matt Smith <mss433@cornell.edu>
2021-04-26 18:58:32 -0400

    further implemented drawing

--- windowDisplay.ml
+++ windowDisplay.ml
@@ -13,21 +13,22 @@ module Vec2 = struct
 
   let vec_of_int_tpl (x, y) : vec = (float_of_int x, float_of_int y)
 
-  let zero = vec_of_float 0.
-
-  let one = vec_of_float 1.
-
-  let swap ((x, y) : vec) = vec_of_floats y x
-
   let x_of ((x, y) : vec) = x
-
-  let x_int_of ((x, y) : vec) = int_of_float x
 
   let y_of ((x, y) : vec) = y
 
+  let x_int_of ((x, y) : vec) = int_of_float x
+
   let y_int_of ((x, y) : vec) = int_of_float y
 
-  let ints_of_vec ((x, y) : vec) = (int_of_float x, int_of_float y)
+  let map (f : float -> 'a) ((x, y) : vec) : 'a * 'a = (f x, f y)
+
+  let ints_of_vec = map int_of_float
+
+  let strings_of_vec = map string_of_float
+
+  let int_strings_of_vec =
+    map (fun fl -> fl |> int_of_float |> string_of_int)
 
   let element_wise_op (op : float -> float -> float) v1 v2 : vec =
     let x = op (x_of v1) (x_of v2) in
@@ -40,11 +41,13 @@ module Vec2 = struct
 
   let ( *.. ) = element_wise_op ( *. )
 
-  let scale s = ( *.. ) (vec_of_float s)
+  let scale s = map (( *. ) s)
 
   let scale_x s = ( *.. ) (vec_of_floats s 1.)
 
   let scale_y s = ( *.. ) (vec_of_floats 1. s)
+
+  let swap ((x, y) : vec) = vec_of_floats y x
 
   (** [rotate a v] is v rotated counterclockwise around the origin by
       [a] radians *)
@@ -54,38 +57,44 @@ module Vec2 = struct
     let new_x = (x *. cos_a) -. (y *. sin_a) in
     let new_y = (x *. sin_a) +. (y *. cos_a) in
     (new_x, new_y)
+
+  let distance ((x1, y1) : vec) ((x2, y2) : vec) =
+    let xd = x1 -. x2 in
+    let yd = y1 -. y2 in
+    sqrt ((xd *. xd) +. (yd *. yd))
+
+  let cube_round ((x, y) : vec) =
+    let z = -.x -. y in
+    let rx = x |> Float.round in
+    let ry = y |> Float.round in
+    let rz = z |> Float.round in
+
+    let x_diff = Float.abs (rx -. x) in
+    let y_diff = Float.abs (ry -. y) in
+    let z_diff = Float.abs (rz -. z) in
+    let rx =
+      if x_diff > y_diff && x_diff > z_diff then -.ry -. rz else rx
+    in
+    let ry = if y_diff > z_diff then -.rx -. rz else ry in
+    (rx, ry)
 end
 
 open Vec2
 
 let sqrt_3 = sqrt 3.
 
+let screen_size = vec_of_ints 1220 1080
+
 let hex_size = 110.
 
 let hex_spacing = vec_of_floats (sqrt_3 *. hex_size) (1.5 *. hex_size)
 
 let board_size =
-  vec_of_floats (5. *. x_of hex_spacing) (31. /. 4. *. hex_size)
-
-let screen_size = vec_of_ints 1220 1080
+  vec_of_floats (5. *. sqrt_3 *. hex_size) (31. /. 4. *. hex_size)
 
 let board_pos =
-  screen_size -.. board_size
-  |> scale 0.5
-  |> ( +.. ) (vec_of_floats (x_of hex_spacing *. 0.5) hex_size)
-
-let config =
-  " "
-  ^ (screen_size |> x_int_of |> string_of_int)
-  ^ "x"
-  ^ (screen_size |> y_int_of |> string_of_int)
-  ^ "+700-200"
-
-let bg_color = rgb 52 143 235
-
-let token_color = rgb 230 230 197
-
-let render () = Graphics.synchronize ()
+  let offset = vec_of_floats (sqrt_3 *. hex_size) (2. *. hex_size) in
+  screen_size +.. offset -.. board_size |> scale 0.5
 
 let color_of_hex = function
   | Types.Desert -> rgb 212 212 133
@@ -98,86 +107,27 @@ let color_of_hex = function
       | Types.Ore -> rgb 123 111 131)
 
 let draw_string_centered pos text =
-  let draw_pos =
-    pos -.. (text |> text_size |> vec_of_int_tpl |> scale 0.5)
+  let x, y =
+    text |> text_size |> vec_of_int_tpl |> scale 0.5 |> ( -.. ) pos
+    |> ints_of_vec
   in
-  moveto (x_int_of draw_pos) (y_int_of draw_pos);
+  moveto x y;
   draw_string text
 
 (** [set_font_size s] sets the size of drawn text to s. Uses a method
-    described at https://discuss.ocaml.org/t/graphics-set-font-size/2752
-    becuase [Graphics.set_text_size s] does not work. *)
+    referenced at
+    https://discuss.ocaml.org/t/graphics-set-font-size/2752 becuase
+    [Graphics.set_text_size s] does not work. *)
 let set_font_size size =
   "-*-fixed-medium-r-semicondensed--"
   ^ (size |> int_of_float |> string_of_int)
   ^ "-*-*-*-*-*-iso8859-1"
   |> set_font
 
-let unit_hexagon_coords : Vec2.vec array =
-  [|
-    (0., 1.);
-    (sqrt_3 /. 2., 0.5);
-    (sqrt_3 /. 2., -0.5);
-    (0., -1.);
-    (sqrt_3 /. -2., -0.5);
-    (sqrt_3 /. -2., 0.5);
-  |]
-
-let fill_hex pos hex =
-  hex |> color_of_hex |> set_color;
-  let map_coord (v : vec) = scale hex_size v +.. pos |> ints_of_vec in
-  let vertex_coords = unit_hexagon_coords |> Array.map map_coord in
-  vertex_coords |> fill_poly;
-  set_color black;
-  vertex_coords |> draw_poly;
-  match Types.number_of_hex hex with
-  | 7 -> ()
-  | n ->
-      set_color token_color;
-      fill_circle (x_int_of pos) (y_int_of pos)
-        (int_of_float (hex_size /. 3.));
-      set_color black;
-      set_font_size (hex_size *. 0.4);
-      n |> string_of_int |> draw_string_centered pos
-
-let clear color =
-  Graphics.set_color color;
-  let w, h = Vec2.ints_of_vec (screen_size -.. vec_of_ints 1 1) in
-  Graphics.fill_rect 0 0 w h
-
-let coords_of_hex_index i =
-  let v = i |> Board.hex_coords |> vec_of_int_tpl |> swap in
-  let row_shift =
-    if y_int_of v mod 2 == 0 then (0., 0.)
-    else (x_of hex_spacing /. -2., 0.)
-  in
-  let x, y = (v *.. hex_spacing) +.. row_shift in
-  (x, (y_of screen_size *. 0.6) -. y)
-
-let setup_window () =
-  Graphics.open_graph config;
-  Graphics.set_window_title "OCaml Catan";
-  Graphics.auto_synchronize false;
-  clear bg_color
-
-let add_peices (board : Board.t) =
-  let redPlayer = Player.make_player Types.Red in
-  let bluePlayer = Player.make_player Types.Blue in
-  board
-  |> Board.add_settlement redPlayer 13 3
-  |> Board.add_road redPlayer 13 3
-  |> Board.add_settlement redPlayer 9 1
-  |> Board.add_road redPlayer 9 0
-  |> Board.add_road redPlayer 9 5
-  |> Board.add_settlement bluePlayer 9 4
-  |> Board.add_road bluePlayer 9 3
-  |> Board.add_settlement bluePlayer 7 1
-  |> Board.add_road bluePlayer 7 0
-
-(** [indicies_to_draw i] is a list of vertex/edge indicies which hex at
-    index [i] is responsible for drawing. All hexes draw the three
+(** [indicies_to_draw i] is a list of vertex/edge indicies which the hex
+    at index [i] is responsible for drawing. All hexes draw the three
     verticies on their top half, but the bottom 3 verticies are draw
-    conditionall for certain hexes to avoid any vertex being drawn
+    conditionally for certain hexes to avoid any vertex being drawn
     twice. Hexes are aslo responsible for drawing the same indexed edges
     (roads) as they are for verticies. *)
 let indicies_to_draw i =
@@ -193,6 +143,48 @@ let indicies_to_draw i =
   | 12 -> [ 4 ]
   | _ -> []
 
+let unit_hexagon_coords : Vec2.vec array =
+  [|
+    (0., 1.);
+    (sqrt_3 /. 2., 0.5);
+    (sqrt_3 /. 2., -0.5);
+    (0., -1.);
+    (sqrt_3 /. -2., -0.5);
+    (sqrt_3 /. -2., 0.5);
+  |]
+
+let coords_of_hex_index i =
+  let v = i |> Board.hex_coords |> vec_of_int_tpl |> swap in
+  let row_shift =
+    if y_int_of v mod 2 == 0 then (0., 0.)
+    else (x_of hex_spacing /. -2., 0.)
+  in
+  let x, y = (v *.. hex_spacing) +.. row_shift in
+  (x, (y_of screen_size *. 0.6) -. y)
+
+let hex_index_of_mouse_pos pos =
+  let x, y = pos -.. board_pos in
+  let q = ((sqrt_3 /. 3. *. x) -. (1. /. 3. *. y)) /. hex_size in
+  let r = 2. /. 3. *. y /. hex_size in
+  vec_of_floats q r
+
+let fill_hex pos hex =
+  hex |> color_of_hex |> set_color;
+  let map_coord (v : vec) = scale hex_size v +.. pos |> ints_of_vec in
+  let vertex_coords = unit_hexagon_coords |> Array.map map_coord in
+  vertex_coords |> fill_poly;
+  set_color black;
+  vertex_coords |> draw_poly;
+  match Types.number_of_hex hex with
+  | 7 -> ()
+  | n ->
+      set_color (rgb 230 230 197);
+      fill_circle (x_int_of pos) (y_int_of pos)
+        (int_of_float (hex_size /. 3.));
+      set_color black;
+      set_font_size (hex_size *. 0.4);
+      n |> string_of_int |> draw_string_centered pos
+
 let fill_hexes (board : Board.t) =
   for i = 0 to 18 do
     let pos = board_pos +.. coords_of_hex_index i in
@@ -200,6 +192,7 @@ let fill_hexes (board : Board.t) =
     fill_hex pos hex
   done
 
+(** maps team colors to their rgb equivalents *)
 let color_of_team_color = function
   | Types.Red -> rgb 194 14 8
   | Types.Orange -> rgb 235 160 21
@@ -234,22 +227,21 @@ let fill_vertex hex_pos dir (vertex : Types.vertex) =
       fill_circle (x_int_of pos) (y_int_of pos)
         (hex_size *. 0.2 |> int_of_float)
 
-(** [y_of road_size] is the portion of the distance from one vertex to
-    another which a road occupies. [x_of road_size] is the width of a
-    road in the same absolute units as [y_of road_size]. *)
-let road_size = vec_of_floats 0.13 0.6
+(** [y_of road_size] is one half the portion of the distance from one
+    vertex to another which a road occupies. [x_of road_size] is one
+    half the width of a road in the same absolute units as
+    [y_of road_size]. *)
+let road_size = vec_of_floats 0.065 0.3
 
-let half_road_size = scale 0.5 road_size
-
-let unit_edge_coords : Vec2.vec array =
+let unit_road_coords : Vec2.vec array =
   [|
-    half_road_size;
-    scale_y (-1.) half_road_size;
-    scale (-1.) half_road_size;
-    scale_x (-1.) half_road_size;
+    road_size;
+    scale_y (-1.) road_size;
+    scale (-1.) road_size;
+    scale_x (-1.) road_size;
   |]
 
-let edge_verts hex_pos dir =
+let road_verts hex_pos dir =
   let edge_pos =
     unit_hexagon_coords.(dir) +.. unit_hexagon_coords.((dir + 1) mod 6)
     |> scale (0.5 *. hex_size)
@@ -260,13 +252,13 @@ let edge_verts hex_pos dir =
     |> rotate ((dir - 1 |> float_of_int) *. 3.141592 /. -3.)
     |> scale hex_size |> ( +.. ) edge_pos |> ints_of_vec
   in
-  Array.map map_coord unit_edge_coords
+  Array.map map_coord unit_road_coords
 
 let fill_edge hex_pos dir = function
   | Types.Empty -> ()
   | Types.Road c ->
       c |> color_of_team_color |> set_color;
-      let verts = edge_verts hex_pos dir in
+      let verts = road_verts hex_pos dir in
       fill_poly verts;
       set_color black;
       draw_poly verts
@@ -284,11 +276,74 @@ let fill_edges_and_verts (board : Board.t) =
       (indicies_to_draw i)
   done
 
+let clear color =
+  Graphics.set_color color;
+  let w, h = Vec2.ints_of_vec (screen_size -.. vec_of_ints 1 1) in
+  Graphics.fill_rect 0 0 w h
+
+let add_peices (board : Board.t) =
+  let rd = Player.make_player Types.Red in
+  let bl = Player.make_player Types.Blue in
+  board
+  |> Board.add_settlement rd 13 3
+  |> Board.add_road rd 13 3
+  |> Board.add_settlement rd 9 1
+  |> Board.add_road rd 9 0 |> Board.add_road rd 9 5
+  |> Board.add_settlement bl 9 4
+  |> Board.add_road bl 9 3
+  |> Board.add_settlement bl 7 1
+  |> Board.add_road bl 7 0
+
+let render () = Graphics.synchronize ()
+
+let rec wait_click_end start_pos : vec option =
+  let { mouse_x; mouse_y; button } =
+    wait_next_event [ Button_up; Mouse_motion ]
+  in
+  let pos = vec_of_ints mouse_x mouse_y in
+  if distance start_pos pos > 10. then None
+  else if button_down () = false then Some pos
+  else wait_click_end start_pos
+
+let wait_click_start () : vec option =
+  let { mouse_x; mouse_y; button } = wait_next_event [ Button_down ] in
+  (mouse_x, mouse_y) |> vec_of_int_tpl |> wait_click_end
+
+let rec wait_next_click () =
+  match wait_click_start () with
+  | None -> wait_next_click ()
+  | Some c -> c
+
+let rec print_clicks () =
+  let x, y =
+    wait_next_click () |> hex_index_of_mouse_pos |> int_strings_of_vec
+  in
+  print_endline (x ^ ", " ^ y);
+  print_clicks ()
+
 let print_board (board : Board.t) =
   (* for debuggin purposes, this adds peices to the board *)
   let board = add_peices board in
-  setup_window ();
+
+  let x, y = int_strings_of_vec screen_size in
+  Graphics.open_graph (" " ^ x ^ "x" ^ y ^ "+700-200");
+  Graphics.set_window_title "OCaml Catan";
+  Graphics.auto_synchronize false;
+  clear (rgb 52 143 235);
   fill_hexes board;
   fill_edges_and_verts board;
+  let res = 2 in
+  (* for x = 0 to x_int_of screen_size / res do
+    for y = 0 to y_int_of screen_size / res do
+      if (x + y) mod 2 = 0 then (
+        let pixel_pos = vec_of_ints (res * x) (res * y) in
+        let unrounded = pixel_pos |> hex_index_of_mouse_pos in
+        let rounded = map Float.round unrounded in
+        let dist = abs_float (distance unrounded rounded) in
+        set_color (rgb 100 100 (255. *. dist |> int_of_float))
+        (* fill_rect (x_int_of pixel_pos) (y_int_of pixel_pos) res res) *)
+    done
+  done; *)
   render ();
-  Graphics.loop_at_exit [] ignore
+  (* Graphics.loop_at_exit [] ignore *)
+  print_clicks () |> ignore
