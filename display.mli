f286ba6a2d5aeac96e6efcff4e40f06854091f35
Matt Smith <mss433@cornell.edu>
2021-05-22 12:06:46 -0400

    commented display functions

--- display.mli
+++ display.mli
@@ -6,8 +6,11 @@ val is_window_open : unit -> bool
 
 val next_board_click : unit -> Types.boardClick
 
+(** returns the hex index clicked *)
 val next_hex_click : unit -> int
 
+(** returns (hex_index, edge_dir) *)
 val next_edge_click : unit -> int * int
 
+(** returns (hex_index, vert_dir) *)
 val next_vert_click : unit -> int * int
